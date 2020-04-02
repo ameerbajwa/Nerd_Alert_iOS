5eaf6df57d99be837bea1cc782825e97e257e948
Ameer Bajwa <ameerbajwa@gmail.com>
2020-04-01 22:58:38 -0700

    Created simple design for the welcome page

---
+++ Nerd_Alert_iOS/Controllers/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Nerd_Alert_iOS
+//
+//  Created by Ameer Bajwa on 4/1/20.
+//  Copyright © 2020 Ameer Bajwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        logInButton.layer.cornerRadius = 10
+        logInButton.layer.borderWidth = 3
+        signUpButton.layer.cornerRadius = 10
+        signUpButton.layer.borderWidth = 3
+        
+    }
+    
+    @IBAction func logInPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func signUpPressed(_ sender: UIButton) {
+    }
+    
+
+}
+
