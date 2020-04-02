3ccaba906c7a7e9b04b13e0235d604e322e5cf1d
Ameer Bajwa <ameerbajwa@gmail.com>
2020-04-01 22:42:23 -0700

    Initial Commit

---
+++ Nerd_Alert_iOSTests/Nerd_Alert_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Nerd_Alert_iOSTests.swift
+//  Nerd_Alert_iOSTests
+//
+//  Created by Ameer Bajwa on 4/1/20.
+//  Copyright © 2020 Ameer Bajwa. All rights reserved.
+//
+
+import XCTest
+@testable import Nerd_Alert_iOS
+
+class Nerd_Alert_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
