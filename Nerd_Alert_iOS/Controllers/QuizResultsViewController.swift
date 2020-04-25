1f107ac47e0fa4fe23284e274dc2b4301d1ee7b2
Ameer Bajwa <ameerbajwa@gmail.com>
2020-04-25 13:48:34 -0700

    Incorporated the logic for the QuizResultsViewController to pass the users' quiz results and question results to the backend of the application to be inserted into the database. Now user will be able to view his score after they take the quiz

--- Nerd_Alert_iOS/Controllers/QuizResultsViewController.swift
+++ Nerd_Alert_iOS/Controllers/QuizResultsViewController.swift
@@ -19,7 +19,12 @@ class QuizResultsViewController: UIViewController {
     var quizQuestions: [QuizQuestion] = []
     var usersAnswers: [Int: String?] = [:]
     
+    var quizQuestionsResultsService = QuizQuestionsResultsService()
+    var quizResultsService = QuizResultsService()
+    
     var quizQuestionsResults: [QuizQuestionsResults] = []
+    var quizResults: QuizResults?
+    var score: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,10 +38,37 @@ class QuizResultsViewController: UIViewController {
             quizQuestionsResults[i].questionId = quizQuestions[i].id
             quizQuestionsResults[i].userAnswer = usersAnswers[i]!!
             quizQuestionsResults[i].correctAnswer = quizQuestions[i].correctAnswer
+            
+            if usersAnswers[i]!! == quizQuestions[i].correctAnswer {
+                score += 1
+            }
+            
         }
         
         print("Quiz Question Results")
         print(quizQuestionsResults)
+        
+        quizResultsService.injectQuizResult(user!.id, quiz!.id, 1, score, onSuccess: { (response) in
+            print("Successfully entered user's score for this quiz")
+            print(response)
+            
+            self.usernameLabel.text = "\(self.user!.username) received"
+            self.scoreLabel.text = "\(self.score)/10"
+            self.quizNameLabel.text = "on \(self.quiz!.name) quiz"
+            
+            DispatchQueue.main.async{
+                self.quizQuestionsResultsService.injectQuizQuestionsResults(self.quizQuestionsResults, onSuccess: { (response) in
+                    print("Successfully enterd user's answers to each question for this quiz")
+                }, onFailure: { (error) in
+                    print("Error in injecting users' answers for this quiz \(self.quiz!.name)")
+                    print(error)
+                })
+            }
+            
+        }, onFailure: { (error) in
+            print("Error in injecting quiz result for user \(self.user!.username)")
+            print(error)
+        })
         
         
     }
---
+++ Nerd_Alert_iOS/Models/Services/QuizResultsService.swift
@@ -0,0 +1,52 @@
+//
+//  QuizResultsService.swift
+//  Nerd_Alert_iOS
+//
+//  Created by Ameer Bajwa on 4/25/20.
+//  Copyright © 2020 Ameer Bajwa. All rights reserved.
+//
+
+import Foundation
+
+class QuizResultsService {
+    
+    let restAPIManager = RestAPIManager()
+    
+    func injectQuizResult(_ userId: Int, _ quizId: Int, _ quizIteration: Int, _ score: Int, onSuccess: @escaping ([String: Any]) -> Void, onFailure: @escaping ([String: Any]) -> Void) {
+        let commandURL = "/enter_user_quiz_results"
+        
+        let jsonBody: [String: Any] = ["user_id": userId,
+                                       "quiz_id": quizId,
+                                       "quiz_iteration": quizIteration,
+                                       "score": score]
+        let jsonData = try? JSONSerialization.data(withJSONObject: jsonBody)
+        print(jsonData)
+        
+        restAPIManager.httpRequest(url: commandURL, body: jsonData, method: "POST",
+                                   onSuccess: { responseJSON -> Void in
+                                       onSuccess(responseJSON)
+                                   },
+                                   onFailure: { responseJSON -> Void in
+                                       onFailure(responseJSON)
+                                   })
+    }
+    
+    func retrieveQuizResults(_ userId: Int, _ quizId: Int, onSuccess: @escaping ([String: Any]) -> Void, onFailure: @escaping ([String: Any]) -> Void) {
+        let commandURL = "retrieve_user_quiz_results"
+        
+        let jsonBody: [String: Int] = ["user_id": userId,
+                                       "quiz_id": quizId]
+        let jsonData = try? JSONSerialization.data(withJSONObject: jsonBody)
+        print(jsonData)
+        
+        restAPIManager.httpRequest(url: commandURL, body: jsonData, method: "POST",
+                                   onSuccess: { responseJSON -> Void in
+                                       onSuccess(responseJSON)
+                                   },
+                                   onFailure: { responseJSON -> Void in
+                                       onFailure(responseJSON)
+                                   })
+        
+    }
+    
+}
