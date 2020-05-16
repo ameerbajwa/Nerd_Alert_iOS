755d504ea133a170f9f9b9a1000d51b6a3ef3a1d
Ameer Bajwa <ameerbajwa@gmail.com>
2020-05-16 15:50:07 -0700

    Added new view controller which the user can see questions from a previous quiz iteration they have already taken to see the correct answers of that quiz iteration.

---
+++ Nerd_Alert_iOS/Controllers/QuizQuestionResultsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QuizQuestionResultsViewController.swift
+//  Nerd_Alert_iOS
+//
+//  Created by Ameer Bajwa on 5/16/20.
+//  Copyright © 2020 Ameer Bajwa. All rights reserved.
+//
+
+import UIKit
+
+class QuizQuestionResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
