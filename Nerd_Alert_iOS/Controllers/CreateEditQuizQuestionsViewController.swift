b79d095abea59d3a5826b2a9e170aeb63e0087e8
Ameer Bajwa <ameerbajwa@gmail.com>
2020-05-21 23:22:53 -0700

    Finished creating the API calls for creating and updating quiz questions on the QuizQuestionService and completed the flow of functionality for creating and editing quiz questions between the front-end to the back-end

--- Nerd_Alert_iOS/Controllers/CreateEditQuizQuestionsViewController.swift
+++ Nerd_Alert_iOS/Controllers/CreateEditQuizQuestionsViewController.swift
@@ -23,27 +23,63 @@ class CreateEditQuizQuestionsViewController: UIViewController {
     var user_id: Int?
     
     var quizQuestionService = QuizQuestionSerivce()
+    var quizQuestion: QuizQuestion?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        if let questionId = question_id {
+            actionButton.setTitle("Save Question", for: .normal)
+            quizQuestionService.retrieveQuizQuestion(question_id: questionId, onSuccess: { (response) in
+                print("retrieveQuizQuestion API call successful")
+                print(response)
+                
+                self.quizQuestion = QuizQuestion(json: response)
+                
+                DispatchQueue.main.async {
+                    self.questionTextField.text = self.quizQuestion?.question
+                    self.choiceATextField.text = self.quizQuestion?.choiceA
+                    self.choiceBTextField.text = self.quizQuestion?.choiceC
+                    self.choiceCTextField.text = self.quizQuestion?.choiceC
+                    self.choiceDTextField.text = self.quizQuestion?.choiceD
+                    self.correctAnswerTextField.text = self.quizQuestion?.correctAnswer
+                }
+                
+            }, onFailure: {(error) in
+                print("ERROR retrieveQuizQuestion API call unsucessful")
+                print(error)
+            })
+        } else {
+            actionButton.setTitle("Add Question", for: .normal)
+        }
 
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     @IBAction func cancelButtonPressed(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
     }
     
     @IBAction func actionButtonPressed(_ sender: UIButton) {
+        if questionTextField.text != "" && choiceATextField.text != "" && choiceBTextField.text != "" && choiceCTextField.text != "" && choiceDTextField.text != "" && correctAnswerTextField.text != "" {
+            
+            if actionButton.titleLabel?.text == "Add Question" {
+                quizQuestionService.injectQuizQuestion(quiz_id!, questionTextField.text!, choiceATextField.text!, choiceBTextField.text!, choiceCTextField.text!, choiceDTextField.text!, correctAnswerTextField.text!, onSuccess: { (response) in
+                    print("generateQuizQuestion API call successful")
+                    self.navigationController?.popViewController(animated: true)
+                }, onFailure: { (error) in
+                    print("ERROR generateQuizQuestion API call unsuccessful")
+                    print(error)
+                })
+            } else if actionButton.titleLabel?.text == "Save Question" {
+                quizQuestionService.reviseQuizQuestion(question_id!, questionTextField.text!, choiceATextField.text!, choiceBTextField.text!, choiceCTextField.text!, choiceDTextField.text!, correctAnswerTextField.text!, onSuccess: { (response) in
+                    print("reviseQuizQuestion API call successful")
+                    self.navigationController?.popViewController(animated: true)
+                }, onFailure: { (error) in
+                    print("ERROR reviseQuizQuestion API call unsuccessful")
+                    print(error)
+                })
+            }
+            
+        }
     }
 }
--- Nerd_Alert_iOS/Models/Services/QuizQuestionService.swift
+++ Nerd_Alert_iOS/Models/Services/QuizQuestionService.swift
@@ -12,6 +12,29 @@ class QuizQuestionSerivce {
     
     let restAPIManager = RestAPIManager()
     
+    func injectQuizQuestion(_ quiz_id: Int, _ question: String, _ choice_A: String, _ choice_B: String, _ choice_C: String, _ choice_D: String, _ correct_answer: String, onSuccess: @escaping ([String: Any]) -> Void, onFailure: @escaping ([String: Any]) -> Void) {
+        let commandURL = "/generate_quiz_questions"
+        
+        let jsonBody: [String: Any] = [
+                                        "question": question,
+                                        "choice_A": choice_A,
+                                        "choice_B": choice_B,
+                                        "choice_C": choice_C,
+                                        "choice_D": choice_D,
+                                        "correct_answer": correct_answer
+                                      ]
+        let jsonData = try? JSONSerialization.data(withJSONObject: jsonBody)
+        
+        restAPIManager.httpRequest(url: commandURL, body: jsonData, method: "POST",
+                                   onSuccess: { responseJSON -> Void in
+                                       onSuccess(responseJSON)
+                                   },
+                                   onFailure: { responseJSON -> Void in
+                                       onFailure(responseJSON)
+                                   })
+    }
+    
+    
     func retrieveQuizQuestions(_ quiz_id: Int, _ user_id: Int, onSuccess: @escaping ([String: Any]) -> Void, onFailure: @escaping ([String: Any]) -> Void) {
         let commandURL = "/retrieve_quiz_questions"
         
@@ -19,6 +42,40 @@ class QuizQuestionSerivce {
         let jsonData = try? JSONSerialization.data(withJSONObject: jsonBody)
         
         restAPIManager.httpRequest(url: commandURL, body: jsonData, method: "POST",
+                                   onSuccess: { responseJSON -> Void in
+                                       onSuccess(responseJSON)
+                                   },
+                                   onFailure: { responseJSON -> Void in
+                                       onFailure(responseJSON)
+                                   })
+    }
+    
+    func retrieveQuizQuestion(question_id: String, onSuccess: @escaping ([String: Any]) -> Void, onFailure: @escaping ([String: Any]) -> Void) {
+        let commandURL = "/retrieve_quiz_question/questionId/\(question_id)"
+        
+        restAPIManager.httpRequest(url: commandURL, body: nil, method: "GET",
+                           onSuccess: { responseJSON -> Void in
+                               onSuccess(responseJSON)
+                           },
+                           onFailure: { responseJSON -> Void in
+                               onFailure(responseJSON)
+                           })
+    }
+    
+    func reviseQuizQuestion(_ question_id: String, _ question: String, _ choice_A: String, _ choice_B: String, _ choice_C: String, _ choice_D: String, _ correct_answer: String, onSuccess: @escaping ([String: Any]) -> Void, onFailure: @escaping ([String: Any]) -> Void) {
+        let commandURL = "/update_quiz_question/questionId/\(question_id)"
+        
+        let jsonBody: [String: Any] = [
+                                        "question": question,
+                                        "choice_A": choice_A,
+                                        "choice_B": choice_B,
+                                        "choice_C": choice_C,
+                                        "choice_D": choice_D,
+                                        "correct_answer": correct_answer
+                                      ]
+        let jsonData = try? JSONSerialization.data(withJSONObject: jsonBody)
+        
+        restAPIManager.httpRequest(url: commandURL, body: jsonData, method: "PUT",
                                    onSuccess: { responseJSON -> Void in
                                        onSuccess(responseJSON)
                                    },
