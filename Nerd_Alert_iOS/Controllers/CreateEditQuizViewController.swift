dd33f93ecb13939897b2a914abace5a19da18b02
Ameer Bajwa <ameerbajwa@gmail.com>
2020-05-24 23:37:59 -0700

    Inserted a scrollview into the createeditquiz view controller so when a keyboard pops up, the view will scroll accordingly to the top of the screen so the keyboard won't hide anything.

--- Nerd_Alert_iOS/Controllers/CreateEditQuizViewController.swift
+++ Nerd_Alert_iOS/Controllers/CreateEditQuizViewController.swift
@@ -16,6 +16,7 @@ class CreateEditQuizViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var quizDescriptionTextView: UITextView!
         
     @IBOutlet weak var quizActionButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     var user: User?
     var action: String?
@@ -28,6 +29,13 @@ class CreateEditQuizViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.setHidesBackButton(true, animated: true)
+        
+//        NotificationCenter.default.addObserver(self, selector: #selector(Keyboard), name: Notification.Name.UIResponder.keyboardWillHideNotification, object: nil)
+//        NotificationCenter.default.addObserver(self, selector: #selector(Keyboard), name: Notification.Name.UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
         
         quizNameTextView.creatingPresentableTextFields(textView: quizNameTextView)
         titleOfSourceTextView.creatingPresentableTextFields(textView: titleOfSourceTextView)
@@ -62,6 +70,23 @@ class CreateEditQuizViewController: UIViewController, UITextFieldDelegate {
         } else {
             quizActionButton.setTitle("Submit Quiz Details", for: .normal)
         }
+        
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            scrollView.contentInset = .zero
+        } else {
+            scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+
+        scrollView.scrollIndicatorInsets = scrollView.contentInset
         
     }
     
