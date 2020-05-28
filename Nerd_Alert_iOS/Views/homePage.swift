d3e4139c27119a46cac67ea74be2de71339f5fc9
Ameer Bajwa <ameerbajwa@gmail.com>
2020-05-27 23:10:32 -0700

    Trying to redesign the buttons on the home page xib file after the user logs in. Changed the quizAction button to a segmented controller. Trying to find a way to find a better design for the create quiz button programmatically, which will become part of an UI button extension file where all the small buttons on every xib file will adhere too.

--- Nerd_Alert_iOS/Views/homePage.swift
+++ Nerd_Alert_iOS/Views/homePage.swift
@@ -16,7 +16,8 @@ import UIKit
 class homePage: UIView {
     
     @IBOutlet weak var usernameLabel: UILabel!
-    @IBOutlet weak var quizActionButton: UIButton!
+    @IBOutlet weak var quizControl: UISegmentedControl!
+//    @IBOutlet weak var quizActionButton: UIButton!
     @IBOutlet weak var createQuizButton: UIButton!
     @IBOutlet var delegate: homePageDelegate?
     
@@ -27,7 +28,17 @@ class homePage: UIView {
     
     func homePageXibInit(username: String) {
         usernameLabel.text = "Home of \(username)"
-        changingButtonTitles()
+        quizControl.setTitle("Quizzes", forSegmentAt: 0)
+        quizControl.setTitle("My Quizzes", forSegmentAt: 1)
+        
+        createQuizButton.layer.borderWidth = 3
+        createQuizButton.layer.cornerRadius = 10
+        createQuizButton.layer.borderColor = UIColor.black.cgColor
+        createQuizButton.titleLabel?.textColor = UIColor.black
+        createQuizButton.frame.size.height = 40
+        createQuizButton.frame.size.width = 90
+        createQuizButton.isHidden = true
+//        changingButtonTitles()
         
 //        if usersQuizzesInstance.usersQuizzes == true {
 //            createQuizButton.isHidden = true
@@ -36,21 +47,36 @@ class homePage: UIView {
 //        }
     }
     
-    func changingButtonTitles() {
-        if usersQuizzesInstance.usersQuizzes == true { // users_quizzes == true
-            quizActionButton.setTitle("View Quizzes", for: .normal)
-            createQuizButton.isHidden = false
-        } else {
-            quizActionButton.setTitle("View My Quizzes", for: .normal)
+//    func changingButtonTitles() {
+//        if usersQuizzesInstance.usersQuizzes == true { // users_quizzes == true
+//            quizActionButton.setTitle("View Quizzes", for: .normal)
+//            createQuizButton.isHidden = false
+//        } else {
+//            quizActionButton.setTitle("View My Quizzes", for: .normal)
+//            createQuizButton.isHidden = true
+//        }
+//    }
+    
+    @IBAction func quizControlChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            usersQuizzesInstance.usersQuizzes = false
+            self.delegate?.retrievingQuizzes(users_quizzes: usersQuizzesInstance.usersQuizzes)
             createQuizButton.isHidden = true
+        case 1:
+            usersQuizzesInstance.usersQuizzes = true
+            self.delegate?.retrievingQuizzes(users_quizzes: usersQuizzesInstance.usersQuizzes)
+            createQuizButton.isHidden = false
+        default:
+            break
         }
     }
     
-    @IBAction func quizActionButtonPressed(_ sender: UIButton) {
-        usersQuizzesInstance.usersQuizzes = !usersQuizzesInstance.usersQuizzes
-        changingButtonTitles()
-        self.delegate?.retrievingQuizzes(users_quizzes: usersQuizzesInstance.usersQuizzes)
-    }
+//    @IBAction func quizActionButtonPressed(_ sender: UIButton) {
+//        usersQuizzesInstance.usersQuizzes = !usersQuizzesInstance.usersQuizzes
+//        changingButtonTitles()
+//        self.delegate?.retrievingQuizzes(users_quizzes: usersQuizzesInstance.usersQuizzes)
+//    }
     
     @IBAction func createQuizButtonPressed(_ sender: UIButton) {
         self.delegate?.createQuiz(action: "Create")
